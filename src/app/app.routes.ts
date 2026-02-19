e57e02c2bae924cd9f846c65ca7c088b6a56896b
pinzii <fpinzon514@gmail.com>
2026-02-18 22:55:16 -0500

    Agrega layout con Material y routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
+import { ProjectsComponent } from './features/projects/pages/projects/projects.component';
+import { AboutComponent } from './features/about/pages/about/about.component';
+import { ContactComponent } from './features/contact/pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+    ],
+   },
+   { path: '**', redirectTo: '' },
+];
--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.scss'
+  styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent {}
 
-}
